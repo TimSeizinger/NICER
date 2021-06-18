f7bc8b8063b8adc45332da0d580bc68cc420cc94
Tim Seizinger <tim.seizinger@stud-mail.uni-wuerzburg.de>
2021-06-18 17:07:12 +0200

    Fix outpu path

--- pexels_editing_test_orientation.py
+++ pexels_editing_test_orientation.py
@@ -21,7 +21,7 @@ if not os.path.isdir("./analysis/results/" + output_file + "/"):
 
 nicer = NICER(config.can_checkpoint_path, config.nima_checkpoint_path)
 
-evaluate_editing_pexels(nicer, 'output_file', 'pexels_wide', limit=10)
+evaluate_editing_pexels(nicer, output_file, 'pexels_wide', limit=10)
 
 '''
 evaluate_pexels(nicer, 'pexels_wide', 'pexels_wide')
