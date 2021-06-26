8c08d9c334f08880cd07c31d10b320ed7bfeb683
Tim Seizinger <tim.seizinger@stud-mail.uni-wuerzburg.de>
2021-06-26 17:22:10 +0200

    Task defined.

--- pexels_editing_test_losses.py
+++ pexels_editing_test_losses.py
@@ -30,8 +30,9 @@ nicer = NICER(config.can_checkpoint_path, config.nima_checkpoint_path)
 
 evaluate_editing_losses_pexels(nicer, output_file, 'pexels_test_1000',
                                ['MSE_SCORE_REG', 'ADAPTIVE_MSE_SCORE_REG', 'MOVING_MSE_SCORE_REG',
-                                'MSE_STYLE_CHANGES_REG', 'MSE_STYLE_CHANGES_HINGE', 'MSE_STYLE_CHANGES_HINGE_REG'],
-                               nima_vgg16=False, nima_mobilenetv2=False, ssmtpiaa=True, ssmtpiaa_fine=False, limit=10)
+                                'MSE_STYLE_CHANGES_REG', 'MSE_STYLE_CHANGES_HINGE', 'MSE_STYLE_CHANGES_HINGE_REG',
+                                'COMPOSITE'],
+                               nima_vgg16=False, nima_mobilenetv2=False, ssmtpiaa=True, ssmtpiaa_fine=False, limit=250)
 
 elapsed_time = time.time() - start_time
 elapsed_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
