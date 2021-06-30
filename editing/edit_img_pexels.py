1cb09d4409e258a48798beb743361247457e81e9
Tim Seizinger <tim.seizinger@stud-mail.uni-wuerzburg.de>
2021-06-30 23:18:49 +0200

    Update edit_img_pexels.py

--- editing/edit_img_pexels.py
+++ editing/edit_img_pexels.py
@@ -49,24 +49,68 @@ pexels = Pexels()
 print("Loaded Pexels dataset")
 ed = preprocess_images.ImageEditor()
 
-for i in range(100):  # len(pexels)
+for i in range(10):  # len(pexels)
     print("editing: " + pexels.__getitem__(i) + 'with')
-    distortions = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
-                   ('shadows', randrange(-1, 4)), ('highlights', randrange(-4, 1)), ('exposure', randrange(-2, 1))]
-    while distortions[0][1] == distortions[1][1] == distortions[2][1] == distortions[3][1] == distortions[4][1] == 0:
-        distortions = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
-                       ('shadows', randrange(-1, 3)), ('highlights', randrange(-3, 1)), ('exposure', randrange(-1, 1))]
+    distortions1 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                    ('shadows', randrange(-1, 4)), ('highlights', randrange(-4, 1)), ('exposure', randrange(-2, 1))]
+    while distortions1[0][1] == distortions1[1][1] == distortions1[2][1] == distortions1[3][1] == distortions1[4][1] == 0:
+        distortions1 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                        ('shadows', randrange(-1, 3)), ('highlights', randrange(-3, 1)), ('exposure', randrange(-1, 1))]
 
-    print(distortions)
+    print(distortions1)
 
     image = Image.open('/' + pexels.__getitem__(i)).convert("RGB")
 
-    for distortion in distortions:
+    for distortion in distortions1:
         image = ed.distort_image(distortion[0], distortion[1], img=image)
 
     save_path = Path('/out/') / \
-                    f"{pexels.__getitem__(i).split('/')[1].split('.')[0]}_{distortions[0][1]}_{distortions[1][1]}_" \
-                    f"{distortions[2][1]}_{distortions[3][1]}_{distortions[4][1]}." \
+                    f"{pexels.__getitem__(i).split('/')[1].split('.')[0]}_{distortions1[0][1]}_{distortions1[1][1]}_" \
+                    f"{distortions1[2][1]}_{distortions1[3][1]}_{distortions1[4][1]}." \
                     f"{pexels.__getitem__(i).split('/')[1].split('.')[1]}"
+    print(f"saving to\t{save_path}")
+    image.save(save_path)
+
+    print("editing: " + pexels.__getitem__(i) + 'with')
+    distortions2 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                    ('shadows', randrange(-1, 4)), ('highlights', randrange(-4, 1)), ('exposure', randrange(-2, 1))]
+    while distortions2[0][1] == distortions2[1][1] == distortions2[2][1] == distortions2[3][1] == distortions2[4][
+        1] == 0 or distortions1 == distortions2:
+        distortions2 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                        ('shadows', randrange(-1, 3)), ('highlights', randrange(-3, 1)), ('exposure', randrange(-1, 1))]
+
+    print(distortions2)
+
+    image = Image.open('/' + pexels.__getitem__(i)).convert("RGB")
+
+    for distortion in distortions2:
+        image = ed.distort_image(distortion[0], distortion[1], img=image)
+
+    save_path = Path('/out/') / \
+                f"{pexels.__getitem__(i).split('/')[1].split('.')[0]}_{distortions2[0][1]}_{distortions2[1][1]}_" \
+                f"{distortions2[2][1]}_{distortions2[3][1]}_{distortions2[4][1]}." \
+                f"{pexels.__getitem__(i).split('/')[1].split('.')[1]}"
+    print(f"saving to\t{save_path}")
+    image.save(save_path)
+
+    print("editing: " + pexels.__getitem__(i) + 'with')
+    distortions3 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                    ('shadows', randrange(-1, 4)), ('highlights', randrange(-4, 1)), ('exposure', randrange(-2, 1))]
+    while distortions3[0][1] == distortions3[1][1] == distortions3[2][1] == distortions3[3][1] == distortions3[4][
+        1] == 0 or distortions1 == distortions3 or distortions2==distortions3:
+        distortions3 = [('saturation', randrange(-1, 1)), ('contrast', randrange(-1, 1)),
+                        ('shadows', randrange(-1, 3)), ('highlights', randrange(-3, 1)), ('exposure', randrange(-1, 1))]
+
+    print(distortions3)
+
+    image = Image.open('/' + pexels.__getitem__(i)).convert("RGB")
+
+    for distortion in distortions3:
+        image = ed.distort_image(distortion[0], distortion[1], img=image)
+
+    save_path = Path('/out/') / \
+                f"{pexels.__getitem__(i).split('/')[1].split('.')[0]}_{distortions3[0][1]}_{distortions3[1][1]}_" \
+                f"{distortions3[2][1]}_{distortions3[3][1]}_{distortions3[4][1]}." \
+                f"{pexels.__getitem__(i).split('/')[1].split('.')[1]}"
     print(f"saving to\t{save_path}")
     image.save(save_path)
