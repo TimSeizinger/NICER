73b281c0b675cb346f7a62f319b2344848c2d094
Tim Seizinger <tim.seizinger@stud-mail.uni-wuerzburg.de>
2021-07-01 09:13:22 +0200

    Hyperparameter first test run

--- hyperparamtest.py
+++ hyperparamtest.py
@@ -25,6 +25,8 @@ parser.add_argument("--composite_balance", type=float, required=True)
 parser.add_argument("--adaptive_score_offset", type=float, required=True)
 args = parser.parse_args()
 
+print(args)
+
 # Initialize output folders
 folder = Path(f"{args.optim_lr}_{args.gamma}_{args.score_pow}_{args.composite_balance}_{args.adaptive_score_offset}")
 out = Path("out")
@@ -46,9 +48,11 @@ nicer.config.composite_balance = args.composite_balance
 nicer.config.adaptive_score_offset = args.adaptive_score_offset
 
 # Process images
+'''
 evaluate_editing_recovery_pexels(nicer=nicer, sample_size=args.sample_size, img_path=out/folder,
                                  graph_data_path=out/folder/data, filename=folder, loss='Composite',
                                  nima_vgg16=False, nima_mobilenetv2=False, ssmtpiaa=True, ssmtpiaa_fine=False)
+'''
 
 elapsed_time = time.time() - start_time
 elapsed_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
